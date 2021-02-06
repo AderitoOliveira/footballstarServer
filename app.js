7c4fccf3a12cfe0764b4deb98d5b0d2acd73d29c
aderito <aderito.nelson1@gmail.com>
2021-02-06 15:39:37 +0000

    Added the erro validation when the file of the viedo is not found

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
 //FILE UPLOAD IMPORTS
-const multer  = require('multer');
+const multer = require('multer');
 const bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
@@ -53,10 +53,10 @@ const storagePlayerExercices = multer.diskStorage({
     const player_id = req.params.player_id
     const dir = './uploads/' + 'player_' + player_id
     fs.exists(dir, exist => {
-    if (!exist) {
-      return fs.mkdir(dir, error => cb(error, dir))
-    }
-    return cb(null, dir)
+      if (!exist) {
+        return fs.mkdir(dir, error => cb(error, dir))
+      }
+      return cb(null, dir)
     })
   },
   filename: (req, file, cb) => {
@@ -66,14 +66,14 @@ const storagePlayerExercices = multer.diskStorage({
     //cb(null, 'UserId-Image-${Date.now()}.png')
 
   }
-  
+
 })
 
 let uploadPlayerExercices = multer({
-   storage: storagePlayerExercices 
+  storage: storagePlayerExercices
 });
 
- 
+
 // POST File
 app.post('/api/upload/:player_id', uploadPlayerExercices.single('image'), function (req, res) {
   if (!req.file) {
@@ -87,87 +87,105 @@ app.post('/api/upload/:player_id', uploadPlayerExercices.single('image'), functi
     console.log('File is available!');
     return res.send({
       success: true
-    }) 
+    })
   }
 });
 
-app.get('/video/:player_id/:video_name', function(req, res) {
-  var player = 'player_' + req.params.player_id;
-  var video = req.params.video_name;
-  const path = './uploads/' + player + '/' + video //'/dribles.mp4'
-  const stat = fs.statSync(path)
-  const fileSize = stat.size
-  const range = req.headers.range
-  
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-")
-    const start = parseInt(parts[0], 10)
-    const end = parts[1]
-    ? parseInt(parts[1], 10)
-    : fileSize-1
-    
-    const chunksize = (end-start)+1
-    const file = fs.createReadStream(path, {start, end})
-    const head = {
-    'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-    'Accept-Ranges': 'bytes',
-    'Content-Length': chunksize,
-    'Content-Type': 'video/mp4',
-    }
-  
-    res.writeHead(206, head)
-    file.pipe(res)
-  } else {
-    const head = {
-    'Content-Length': fileSize,
-    'Content-Type': 'video/mp4',
+app.get('/video/:player_id/:video_name', function (req, res) {
+
+  try {
+
+    var player = 'player_' + req.params.player_id;
+    var video = req.params.video_name;
+    const path = './uploads/' + player + '/' + video //'/dribles.mp4'
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
+
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-")
+      const start = parseInt(parts[0], 10)
+      const end = parts[1]
+        ? parseInt(parts[1], 10)
+        : fileSize - 1
+
+      const chunksize = (end - start) + 1
+      const file = fs.createReadStream(path, { start, end })
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      }
+
+      res.writeHead(206, head)
+      file.pipe(res)
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      }
+
+      res.writeHead(200, head)
+      fs.createReadStream(path).pipe(res)
     }
 
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
+
+  } catch (error) {
+    res.writeHead(404, error)
+    res.end()
+    return
   }
 
 });
 
 
-app.get('/exercisesVideo/:level_id/:video_name', function(req, res) {
-  var exercise_level = req.params.level_id;
-  var video = req.params.video_name;
-  const path = './uploads/exercises_videos/' + 'level_' + exercise_level + '/' + video 
-  const stat = fs.statSync(path)
-  const fileSize = stat.size
-  const range = req.headers.range
-  
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-")
-    const start = parseInt(parts[0], 10)
-    const end = parts[1]
-    ? parseInt(parts[1], 10)
-    : fileSize-1
-    
-    const chunksize = (end-start)+1
-    const file = fs.createReadStream(path, {start, end})
-    const head = {
-    'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-    'Accept-Ranges': 'bytes',
-    'Content-Length': chunksize,
-    'Content-Type': 'video/mp4',
-    }
-  
-    res.writeHead(206, head)
-    file.pipe(res)
-  } else {
-    const head = {
-    'Content-Length': fileSize,
-    'Content-Type': 'video/mp4',
+app.get('/exercisesVideo/:level_id/:video_name', function (req, res) {
+
+  try {
+    var exercise_level = req.params.level_id;
+    var video = req.params.video_name;
+    const path = './uploads/exercises_videos/' + 'level_' + exercise_level + '/' + video
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
+
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-")
+      const start = parseInt(parts[0], 10)
+      const end = parts[1]
+        ? parseInt(parts[1], 10)
+        : fileSize - 1
+
+      const chunksize = (end - start) + 1
+      const file = fs.createReadStream(path, { start, end })
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      }
+
+      res.writeHead(206, head)
+      file.pipe(res)
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      }
+
+      res.writeHead(200, head)
+      fs.createReadStream(path).pipe(res)
     }
 
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
+  } catch (error) {
+    res.writeHead(404, error)
+    res.end()
+    return
   }
 
 });
-  
+
 /*******************************************************   *****************************************************/
 
 
@@ -175,12 +193,12 @@ app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   console.error(err.message);
   if (!err.statusCode) err.statusCode = 500;
